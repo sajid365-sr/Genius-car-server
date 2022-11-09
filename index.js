6eeeb2840cb40095bbc27152d4170faf9bda366f
sajid365-sr <sajidsorker86@gmail.com>
2022-11-10 02:14:18 +0600

    READ operation done

--- index.js
+++ index.js
@@ -1,34 +1,53 @@
-
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require ('dotenv').config();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.90qadcl.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
+async function run() {
+  try {
+    const serviceCollection = client.db("geniusCar").collection("services");
 
+    // READ ALL
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+
+      res.send(services);
+    });
+
+    // READ ONE
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+
+      res.send(service);
+    });
+  } finally {
+  }
+}
+
+run().catch((e) => console.error(e));
 
 // Root
-app.get('/', (req, res) =>{
-    res.send('Car Genius Server Is Running..');
-})
+app.get("/", (req, res) => {
+  res.send("Car Genius Server Is Running..");
+});
 
-app.listen(port, () =>{
-    console.log('Genius Car Server Running On The Port: ', port);
-})
+app.listen(port, () => {
+  console.log("Genius Car Server Running On The Port: ", port);
+});
