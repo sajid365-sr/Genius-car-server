84f26afb95b4500bda4bfa1bd319e87a5da061b9
sajid365-sr <sajidsorker86@gmail.com>
2023-09-02 17:09:31 +0600

    sslcommerz payment system updated

--- index.js
+++ index.js
@@ -3,15 +3,14 @@ const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 const cors = require("cors");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-
 
 // SSL Commerce Integrations
 const store_id = process.env.SSL_STORE_ID;
 const store_password = process.env.SSL_SECRET_KEY;
-const is_live = false //true for live, false for sandbox
-const SSLCommerzPayment = require('sslcommerz-lts');
+const is_live = false; //true for live, false for sandbox
+const SSLCommerzPayment = require("sslcommerz-lts");
 
 // Middleware
 app.use(cors());
@@ -28,21 +27,17 @@ const client = new MongoClient(uri, {
 function verifyJWT(req, res, next) {
   const authHeader = req.headers.authorization;
   if (!authHeader) {
-    return res.status(401).send({ message: 'Unauthorized Access' });
-
+    return res.status(401).send({ message: "Unauthorized Access" });
   }
-  const token = authHeader.split(' ')[1];
+  const token = authHeader.split(" ")[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
     if (err) {
-      return res.status(403).send({ message: 'Forbidden Access' });
+      return res.status(403).send({ message: "Forbidden Access" });
     }
     req.decoded = decoded;
     next();
-  })
+  });
 }
-
-
-
 
 async function run() {
   try {
@@ -50,13 +45,14 @@ async function run() {
     const orderCollection = client.db("geniusCar").collection("orders");
 
     //JWT TOKEN
-    app.post('/jwt', (req, res) => {
+    app.post("/jwt", (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' }); // Assigning JWT token first time after login. Expire time is in ms.
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      }); // Assigning JWT token first time after login. Expire time is in ms.
 
       res.send({ token });
-    })
-
+    });
 
     // READ ALL(GET)
     app.get("/services", async (req, res) => {
@@ -71,67 +67,78 @@ async function run() {
       const search = req.query.search;
 
       let query = {};
-      if (search.length) {
+      if (search) {
         query = {
           $text: {
-            $search: search
-          }
-        }
+            $search: search,
+          },
+        };
       }
-      const order = req.query.order === 'asc' ? 1 : -1;
+      const order = req.query.order === "asc" ? 1 : -1;
       const cursor = serviceCollection.find(query).sort({ price: order });
       const services = await cursor.toArray();
 
       res.send(services);
-
     });
 
-    // READ ONE(GET)
+    // Get specific services by id
     app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const service = await serviceCollection.findOne(query);
+      console.log(id);
+      if (id !== undefined) {
+        const query = { _id: ObjectId(id) };
+        const service = await serviceCollection.findOne(query);
 
-      res.send(service);
+        res.send(service);
+      }
     });
 
-    // READ ALL ORDERS(GET)
-    app.get('/orders', verifyJWT, async (req, res) => {
-
+    // Get customer all orders
+    app.get("/orders", verifyJWT, async (req, res) => {
       const decoded = req.decoded;
 
-
-      if (decoded.email !== req.query.email) { // check if decoded email and login user email are same.
-        res.status(403).send({ message: 'Unauthorized Access' });
+      if (decoded.email !== req.query.email) {
+        // check if decoded email and login user email are same.
+        res.status(403).send({ message: "Unauthorized Access" });
       }
 
       let query = {};
       if (req.query.email) {
         query = {
-          email: req.query.email
-        }
+          email: req.query.email,
+        };
       }
 
       const cursor = orderCollection.find(query);
       const orders = await cursor.toArray();
 
-        // console.log(orders);
       res.send(orders);
-    })
+    });
 
-    // CREATE(POST)
-    app.post("/orders", verifyJWT, async (req, res) => {
+    // CREATE a new order
+    app.post("/orders", async (req, res) => {
       const order = req.body;
-      const { serviceName, customerName, email, address, postcode, currency } = order;
 
-      if (!serviceName || !customerName || !email || !address || !postcode || currency) {
-        return res.send({ error: 'Please provide all the information' });
+      const { serviceName, customerName, email, address, postcode, currency } =
+        order;
+
+      if (
+        !serviceName ||
+        !customerName ||
+        !email ||
+        !address ||
+        !postcode ||
+        !currency
+      ) {
+        return res.send({ error: "Please provide all the information" });
       }
 
-      const orderedService = await serviceCollection.findOne({ _id: ObjectId(order.serviceId) });
+      const orderedService = await serviceCollection.findOne({
+        _id: new ObjectId(order.serviceId),
+      });
       const tnxId = new ObjectId().toString(); // make a unique transaction id
 
-
+      //  Send data to SSLCommerz
       const data = {
         total_amount: orderedService.price,
         currency: order.currency,
@@ -139,71 +146,69 @@ async function run() {
         success_url: `${process.env.SERVER_URL}/payment/success?transactionId=${tnxId}`,
         fail_url: `${process.env.SERVER_URL}/payment/fail?transactionId=${tnxId}`,
         cancel_url: `${process.env.SERVER_URL}/payment/cancel`,
-        ipn_url: 'http://localhost:3030/ipn',
-        shipping_method: 'Courier',
+        ipn_url: "http://localhost:3030/ipn",
+        shipping_method: "Courier",
         product_name: order.serviceName,
-        product_category: 'Electronic',
-        product_profile: 'general',
+        product_category: "Electronic",
+        product_profile: "general",
         cus_name: order.customerName,
         cus_email: order.email,
         cus_add1: order.address,
-        cus_add2: 'Dhaka',
-        cus_city: 'Dhaka',
-        cus_state: 'Dhaka',
+        cus_add2: "Dhaka",
+        cus_city: "Dhaka",
+        cus_state: "Dhaka",
         cus_postcode: order.postcode,
-        cus_country: 'Bangladesh',
+        cus_country: "Bangladesh",
         cus_phone: order.phone,
-        cus_fax: '01711111111',
-        ship_name: 'Customer Name',
-        ship_add1: 'Dhaka',
-        ship_add2: 'Dhaka',
-        ship_city: 'Dhaka',
-        ship_state: 'Dhaka',
+        cus_fax: "01711111111",
+        ship_name: "Customer Name",
+        ship_add1: "Dhaka",
+        ship_add2: "Dhaka",
+        ship_city: "Dhaka",
+        ship_state: "Dhaka",
         ship_postcode: 1000,
-        ship_country: 'Bangladesh',
+        ship_country: "Bangladesh",
       };
 
-
-
-      const sslcz = new SSLCommerzPayment(store_id, store_password, is_live)
-      sslcz.init(data).then(apiResponse => {
+      const sslcz = new SSLCommerzPayment(store_id, store_password, is_live);
+      sslcz.init(data).then((apiResponse) => {
         // Redirect the user to payment gateway
         let GatewayPageURL = apiResponse.GatewayPageURL;
 
         // Save the payment
-        orderCollection.insertOne(({
+        orderCollection.insertOne({
           ...order,
           price: orderedService.price,
           transactionId: tnxId,
           paid: false,
-        }));
-
+        });
 
         res.send({ url: GatewayPageURL });
-
       });
-
-
-    })
+    });
 
     // Payment success route
-    app.post('/payment/success', async (req, res) => {
+    app.post("/payment/success", async (req, res) => {
       const { transactionId } = req.query;
 
       if (!transactionId) {
         return res.redirect(`${process.env.CLIENT_URL}/payment/fail`);
       }
 
-      const result = await orderCollection.updateOne({ transactionId }, { $set: { paid: true, paidAt: new Date().toLocaleString() } });
+      const result = await orderCollection.updateOne(
+        { transactionId },
+        { $set: { paid: true, paidAt: new Date().toLocaleString() } }
+      );
 
       if (result.modifiedCount > 0) {
-        res.redirect(`${process.env.CLIENT_URL}/payment/success?transactionId=${transactionId}`);
+        res.redirect(
+          `${process.env.CLIENT_URL}/payment/success?transactionId=${transactionId}`
+        );
       }
-
     });
 
     // Payment fail route
-    app.post('/payment/fail', async (req, res) => {
+    app.post("/payment/fail", async (req, res) => {
       const { transactionId } = req.query;
 
       if (!transactionId) {
@@ -213,31 +218,25 @@ async function run() {
       if (result.deletedCount) {
         res.redirect(`${process.env.CLIENT_URL}/payment/fail`);
       }
-
-    })
+    });
 
     // Get paid service info
-    app.get('/orders/:transactionId', async (req, res) => {
+    app.get("/orders/:transactionId", async (req, res) => {
       const { transactionId } = req.params;
       const result = await orderCollection.findOne({ transactionId });
 
       res.send(result);
     });
 
-
-    // DELETE 
-    app.delete('/orders/:id', verifyJWT, async (req, res) => {
+    // DELETE
+    app.delete("/orders/:id", verifyJWT, async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await orderCollection.deleteOne(query);
 
       res.send(result);
-
-    })
-
-
+    });
   } finally {
-
   }
 }
 
